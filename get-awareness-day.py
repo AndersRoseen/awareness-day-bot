40169a2731bfad266bbb1bbde6b15c1d82656153
Anders Rosen <andersroseen@gmail.com>
2019-10-27 21:19:25 +0100

    Read themes from file

--- get-awareness-day.py
+++ get-awareness-day.py
@@ -1,2 +1,24 @@
-print("Hello");
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+import json
+from datetime import datetime
+
+todays_date = datetime.today().date().strftime("%d %b %Y")
+# todays_date = '27 Oct 2019'
+
+# Get awareness days
+with open('awareness-days.json', 'r', encoding="utf-8") as f:
+  days_dict = json.load(f)
+  for day in days_dict:
+    day_date = day["Date"]       # datetime.strptime(day["Date"], '%d %b %Y').date()
+    if day_date == todays_date:
+      theme_list = day["Theme"]
+      # break
+
+print(todays_date)
+for theme in theme_list:
+  print(' * ' + theme)
+
+# Post theme_list and todays_date to Slack
+print("Posting to Slack...")
